5233b48d3c30f39c2f51d9d56e7097af3a43f0ba
Taha Hamada <tahahamada2901@gmail.com>
2024-03-09 13:38:57 +0200

    first commit

---
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const bookly());
+}
+
+class bookly extends StatelessWidget {
+  const bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+      );
+  }
+}
+
